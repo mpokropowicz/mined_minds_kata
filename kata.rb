da2d3c46fea55317ff028bcc3ddf7d4f7a204d5c
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-13 12:50:06 -0500

    using ranges instead of times iteration

--- kata.rb
+++ kata.rb
@@ -1,7 +1,12 @@
-puts "1 " * 10
+# puts "1 " * 10
 
-temp = 0
+#  temp = 0
 
-10.times do 
-	puts temp+=1
-end
+#  100.times do 
+#  	puts temp+=1
+# end
+
+#(1...100).each {|num| puts num}
+(1..10).each {|num| print num, ' '}
+puts ''
+(1..10).each {|n| print 1, ' '}
