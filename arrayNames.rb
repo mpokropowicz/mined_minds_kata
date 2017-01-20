1e02d052e22a2c160d19aeda68ef3b113cd70319
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-19 20:14:36 -0500

    added support for incorrect input

--- arrayNames.rb
+++ arrayNames.rb
@@ -1,6 +1,10 @@
 def randomPair(names)
 
-	names = names.shuffle()
+	#we need to make sure our input is correct before we proceed
+	raise "Input needs to be an array" if names.class != Array
+	raise "Array elements must all be of type String" if names.all? {|e| e.class == String} == false
+
+	names = names.shuffle
 	pairs = names.each_slice(2).to_a #let's slice every two elements in names together
 
 	if names.size % 2 != 0 && names.size != 1 #must be odd, can't have only one element
