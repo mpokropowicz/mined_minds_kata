dc7f5b160d4c0d5b8df9239cdd3cc1a9a4e4e057
mpokropowicz <pokropowicz.shinto+git@gmail.com>
2017-01-20 21:23:00 -0500

    appeding group of 3 using '<<'

--- arrayNames.rb
+++ arrayNames.rb
@@ -9,14 +9,14 @@ def randomPair(names)
 
 	if names.size % 2 != 0 && names.size != 1 #must be odd, can't have only one element
 
-		pairs[pairs.size - 2] = pairs[pairs.size - 2] + pairs.last #let's put the group of 3 at the end
+		pairs[pairs.size - 2] << pairs.last[0] #let's put the group of 3 at the end
 		pairs.pop
 	end
 
 	pairs
 end
 
-# print randomPair(["Allen Weber","Brian Lewis","Cummie Washington","Dover Hellfeldt","Edwin Wells","Frank Mugo","Frank Coleman","George Bruner",
-# 			  "Jayvon Harris","John Verbosky","Khalifa Cochran","Lee Brown","Lisa Petrovich","Matt Teitz","Max Pokropowicz","Mike Ciletti",
-# 			  "Pat Wehman","Patrick Roberts","Sherri Dyson","Takhir Salimov","Teela Subba"
-# 			  ])
+print randomPair(["Allen Weber","Brian Lewis","Cummie Washington","Dover Hellfeldt","Edwin Wells","Frank Mugo","Frank Coleman","George Bruner",
+			  "Jayvon Harris","John Verbosky","Khalifa Cochran","Lee Brown","Lisa Petrovich","Matt Teitz","Max Pokropowicz","Mike Ciletti",
+			  "Pat Wehman","Patrick Roberts","Sherri Dyson","Takhir Salimov","Teela Subba"
+			  ])
